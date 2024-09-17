53dff3fee13fec1e97482f8611e5bab7e99b0721
Aria Shi <ariashi15@gmail.com>
2024-09-17 15:19:40 -0400

    08

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function(str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanedStr = str
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
 
+    let start = 0;
+    let end = cleanedStr.length - 1;
+
+    while (start <= end) {
+        if (cleanedStr[start] != cleanedStr[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
