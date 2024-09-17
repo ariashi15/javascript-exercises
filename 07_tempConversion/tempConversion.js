d3536cfcb886d45cca89bc6bb36ef27fb6b58fb4
Aria Shi <ariashi15@gmail.com>
2024-09-17 15:00:54 -0400

    ex 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let res = (f - 32) * 5/9;
+  return parseFloat(res.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let res = c * (9/5) + 32;
+  return parseFloat(res.toFixed(1));
 };
 
 // Do not edit below this line
