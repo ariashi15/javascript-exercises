a0ce13df4b6c048f105832c81eed9c998d1c3499
Aria Shi <ariashi15@gmail.com>
2024-09-17 14:52:54 -0400

    ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    } else if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+    
+    smaller = Math.min(start, end)
+    larger = Math.max(start, end)
+    res = 0;
+    for (let i = smaller; i <= larger; i++) {
+        res += i;
+    }
 
+    return res;
 };
 
 // Do not edit below this line
