c61552d8c724518671151d0f5c3b1ff8ae033b16
Aria Shi <ariashi15@gmail.com>
2024-09-17 15:30:54 -0400

    11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    // res = [];
+    // books.forEach(book => {
+    //     res.push(book.title);
+    // })
 
+    // return res;
+
+    return books.map((book) => book.title);
 };
 
 // Do not edit below this line
