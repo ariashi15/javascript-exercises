4f0a209de1ca76da3cafaf2bcbfdea83d5816276
Aria Shi <ariashi15@gmail.com>
2024-09-17 14:46:18 -0400

    completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let result = [];
+    arr.forEach(element => {
+        if (!args.includes(element)) {
+            result.push(element);
+        }
+    });
+
+    return result;
 };
 
 // Do not edit below this line
