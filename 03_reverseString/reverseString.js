917bddcb92b3841b66a99855739c20c84c888b9d
Aria Shi <ariashi15@gmail.com>
2024-09-17 14:30:17 -0400

    completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(s) {
+    result = "";
+    for (char of s) {
+        result = char + result;
+    }
 
+    return result;
 };
 
 // Do not edit below this line
